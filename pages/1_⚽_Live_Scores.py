4638616f1fa54352126dd6dcc3bcd51d62246ed3
Johnny-Tech <fatormajohn64@gmail.com>
2026-07-06 13:49:16 +0000

    Create 1_⚽_Live_Scores.py

---
+++ pages/1_⚽_Live_Scores.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("⚽ Live Football Scores")
+st.write("Welcome to the Invincible 911 Match Center!")
+
+if st.button("Check Scores"):
+    st.success("The system is ready. Live updates coming soon, insha'Allah!")
+  
